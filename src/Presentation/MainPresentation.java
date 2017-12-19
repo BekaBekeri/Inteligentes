be4e216e9b088cc6afd08646531d8bcc64d29a35
AlvaroGdP <32353297+AlvaroGdP@users.noreply.github.com>
2017-12-19 11:51:40 +0100

    Final commit

--- src/Presentation/MainPresentation.java
+++ src/Presentation/MainPresentation.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * Class Description: Class encharged of presenting the main interface to the user
  * @author Beka Bekeri -, Alvaro Guerrero del Pozo, Fernando Vallejo Banegas
  * Release Date: 29-11-2017
- * @version 5.2.0
+ * @version 5.2.1
  *********************************************************************************/
 public class MainPresentation {
 
